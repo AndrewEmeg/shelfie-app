a46066a2d288ec7431ad3f56694b3bc2fa72be61
Andrew Emeghebo <andrewemeghebo2023@gmail.com>
2024-08-17 18:59:48 -0500

    commit

--- src/pages/sign-in.jsx
+++ src/pages/sign-in.jsx
@@ -133,8 +133,7 @@ function SignIn() {
   return (
     <form
       action=""
-      style={{ height: "calc(var(--vh, 1vh) * 100)" }}
-      className="font-rubik overflow-hidden flex flex-col justify-around px-16 my-0 mx-auto max-w-screen-sm border-2 "
+      className="font-rubik overflow-hidden flex flex-col justify-around py-16 px-8 my-0 mx-auto max-w-screen-sm border-2 "
     >
       <div>
         <h1 className="font-bold max-w-full text-6xl text-teal-700 mb-6">
@@ -160,7 +159,7 @@ function SignIn() {
               (generalError && email === "") || emailError
                 ? "border-red-700 border-2"
                 : "border-gray-400 border"
-            } w-full rounded-lg p-4 block font-light text-2xl border-solid text-gray-800`}
+            } w-full rounded-lg px-4 py-2 block font-light text-2xl border-solid text-gray-800`}
             type="email"
             name="email"
             id="email"
@@ -180,7 +179,7 @@ function SignIn() {
               generalError && password === ""
                 ? "border-red-700 border-2"
                 : "border-gray-400 border"
-            } w-full rounded-lg p-4 block font-light text-2xl border border-solid text-gray-800`}
+            } w-full rounded-lg px-4 py-2 block font-light text-2xl border border-solid text-gray-800`}
             type="password"
             name="password"
             id="password"
@@ -189,7 +188,7 @@ function SignIn() {
 
         <button
           onClick={handleSignInWithEmailAndPassword}
-          className="w-full rounded-lg mt-24 py-6 block font-medium text-3xl text-white bg-teal-700"
+          className="w-full rounded-lg py-4 block font-medium my-24 text-3xl text-white bg-teal-700"
         >
           Sign In
         </button>
